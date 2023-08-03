23277177ec28a5a25360f595f28022c37a38bff6
LoFor1t <vgresnyh@gmail.com>
2023-08-03 14:42:41 +0200

    Initial commit

---
+++ src/main/java/com/LoFor1t/tennisScoreboard/TennisScoreboardApplication.java
@@ -0,0 +1,13 @@
+package com.LoFor1t.tennisScoreboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisScoreboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TennisScoreboardApplication.class, args);
+	}
+
+}
