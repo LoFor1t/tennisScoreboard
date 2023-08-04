8abfd01efc2552c9724d17556367f8473f082748
LoFor1t <vgresnyh@gmail.com>
2023-08-04 14:54:17 +0200

    Make new version of MatchScore.

--- src/main/java/com/LoFor1t/tennisScoreboard/DataModels/MatchScore.java
+++ src/main/java/com/LoFor1t/tennisScoreboard/DataModels/MatchScore.java
@@ -1,10 +1,18 @@
 package com.LoFor1t.tennisScoreboard.DataModels;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class MatchScore extends Match{
-    private int player1SetScore;
-    private int player2SetScore;
+    private int player1Score;
+    private int player2Score;
+    private int player1GameWins;
+    private int player2GameWins;
     private int player1SetWins;
     private int player2SetWins;
+
     public MatchScore(Player player1, Player player2) {
         super(player1, player2);
     }
