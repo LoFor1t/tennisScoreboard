25cca969b1f4427d777b46f7742018703cb0862b
LoFor1t <vgresnyh@gmail.com>
2023-08-09 17:44:40 +0200

    Add saving match to database after it ending.

--- src/main/java/com/LoFor1t/tennisScoreboard/Controllers/MatchScoreController.java
+++ src/main/java/com/LoFor1t/tennisScoreboard/Controllers/MatchScoreController.java
@@ -1,6 +1,8 @@
 package com.LoFor1t.tennisScoreboard.Controllers;
 
+import com.LoFor1t.tennisScoreboard.DataModels.Match;
 import com.LoFor1t.tennisScoreboard.DataModels.MatchScore;
+import com.LoFor1t.tennisScoreboard.DataRepositories.MatchRepository;
 import com.LoFor1t.tennisScoreboard.Services.MatchScoreCalculationService;
 import com.LoFor1t.tennisScoreboard.Services.OngoingMatchesService;
 import org.springframework.stereotype.Controller;
@@ -15,6 +17,12 @@ import java.util.UUID;
 @Controller
 @RequestMapping("/match-score")
 public class MatchScoreController {
+    private final MatchRepository matchRepository;
+
+    public MatchScoreController(MatchRepository matchRepository) {
+        this.matchRepository = matchRepository;
+    }
+
     @GetMapping
     public String matchScorePage(@RequestParam("uuid") String uuid,  ModelMap model) {
         model.addAttribute("match", OngoingMatchesService.getMatchScoreById(UUID.fromString(uuid)));
@@ -26,8 +34,18 @@ public class MatchScoreController {
         MatchScore matchScore = OngoingMatchesService.getMatchScoreById(UUID.fromString(uuid));
         if (player.equals("player-1-win")) {
             new MatchScoreCalculationService(matchScore.getPlayer1Score(), matchScore.getPlayer2Score(), matchScore).addScore();
+            if (matchScore.getPlayer1Score().getSetWins() == 2) {
+                OngoingMatchesService.deleteMatch(UUID.fromString(uuid));
+                Match match = new Match(matchScore.getPlayer1(), matchScore.getPlayer2(), matchScore.getPlayer1());
+                matchRepository.save(match);
+            }
         } else {
             new MatchScoreCalculationService(matchScore.getPlayer2Score(), matchScore.getPlayer1Score(), matchScore).addScore();
+            if (matchScore.getPlayer2Score().getSetWins() == 2) {
+                OngoingMatchesService.deleteMatch(UUID.fromString(uuid));
+                Match match = new Match(matchScore.getPlayer1(), matchScore.getPlayer2(), matchScore.getPlayer2());
+                matchRepository.save(match);
+            }
         }
         model.addAttribute("match", matchScore);
         return "match-score";
--- src/main/java/com/LoFor1t/tennisScoreboard/DataModels/Match.java
+++ src/main/java/com/LoFor1t/tennisScoreboard/DataModels/Match.java
@@ -2,28 +2,39 @@ package com.LoFor1t.tennisScoreboard.DataModels;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Table(name = "matches")
 @Getter
 @Setter
+@NoArgsConstructor
 public class Match {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne
+    @JoinColumn(name = "player1")
     private Player player1;
 
     @ManyToOne
+    @JoinColumn(name = "player2")
     private Player player2;
 
     @ManyToOne
+    @JoinColumn(name = "winner")
     private Player winner;
 
     public Match(Player player1, Player player2) {
         this.player1 = player1;
         this.player2 = player2;
+    }
+
+    public Match(Player player1, Player player2, Player winner) {
+        this.player1 = player1;
+        this.player2 = player2;
+        this.winner = winner;
     }
 }
