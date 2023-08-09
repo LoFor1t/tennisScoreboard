bf3cd34b62c940702120710f16ca1dcb862bd801
LoFor1t <vgresnyh@gmail.com>
2023-08-09 17:30:15 +0200

    Add Tie-Brake functionality and interface for it.

--- src/main/java/com/LoFor1t/tennisScoreboard/Services/MatchScoreCalculationService.java
+++ src/main/java/com/LoFor1t/tennisScoreboard/Services/MatchScoreCalculationService.java
@@ -17,14 +17,16 @@ public class MatchScoreCalculationService {
                 case 0 -> winnerPlayerScore.setScore(15);
                 case 15 -> winnerPlayerScore.setScore(30);
                 case 30 -> winnerPlayerScore.setScore(40);
-                case 40 -> {
-                    addGameWins();
-                }
+                case 40 -> addGameWins();
             }
-        } else if (matchScore.getMatchStatus() == MatchStatus.MoreLess) {
+        } else {
             winnerPlayerScore.setScore(winnerPlayerScore.getScore() + 1);
-            if (winnerPlayerScore.getScore() - loserPlayerScore.getScore() == 2) {
-                addGameWins();
+            if (winnerPlayerScore.getScore() - loserPlayerScore.getScore() >= 2) {
+                if (matchScore.getMatchStatus() == MatchStatus.MoreLess) {
+                    addGameWins();
+                } else if (matchScore.getMatchStatus() == MatchStatus.TieBrake && winnerPlayerScore.getScore() >= 7) {
+                    addSetWins();
+                }
             }
         }
     }
@@ -46,5 +48,28 @@ public class MatchScoreCalculationService {
         winnerPlayerScore.setGameWins(winnerPlayerScore.getGameWins() + 1);
         winnerPlayerScore.setScore(0);
         loserPlayerScore.setScore(0);
+        addSetWins();
+    }
+
+    public void addSetWins() {
+        if (winnerPlayerScore.getGameWins() - loserPlayerScore.getGameWins() >= 2 && winnerPlayerScore.getGameWins() >= 6) {
+            winnerPlayerScore.setGameWins(0);
+            loserPlayerScore.setGameWins(0);
+            winnerPlayerScore.setSetWins(winnerPlayerScore.getSetWins() + 1);
+        } else if (winnerPlayerScore.getGameWins() == 6 && loserPlayerScore.getGameWins() == 6 && matchScore.getMatchStatus() == MatchStatus.Going) {
+            matchScore.setMatchStatus(MatchStatus.TieBrake);
+        } else if (matchScore.getMatchStatus() == MatchStatus.TieBrake) {
+            matchScore.setMatchStatus(MatchStatus.Going);
+            addSetWinAndReset();
+        }
+    }
+
+    public void addSetWinAndReset() {
+        winnerPlayerScore.setGameWins(winnerPlayerScore.getGameWins() + 1);
+        winnerPlayerScore.setScore(0);
+        loserPlayerScore.setScore(0);
+        winnerPlayerScore.setGameWins(0);
+        loserPlayerScore.setGameWins(0);
+        winnerPlayerScore.setSetWins(winnerPlayerScore.getGameWins() + 1);
     }
 }
--- src/main/webapp/WEB-INF/jsp/match-score.jsp
+++ src/main/webapp/WEB-INF/jsp/match-score.jsp
@@ -5,7 +5,9 @@
   Time: 16:44
   To change this template use File | Settings | File Templates.
 --%>
+<%@ taglib uri = "http://java.sun.com/jsp/jstl/core" prefix = "c" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page import="com.LoFor1t.tennisScoreboard.DataModels.MatchStatus" %>
 <html>
 <head>
   <title>Title</title>
@@ -15,7 +17,12 @@
     <table style="text-align: center">
       <tr> <th></th> <th>${match.player1.name}</th> <th>${match.player2.name}</th></tr>
       <tr> <th>Sets</th> <td>${match.player1Score.setWins}</td> <td>${match.player2Score.setWins}</td> </tr>
-      <tr> <th>Games</th> <td>${match.player1Score.gameWins}</td> <td>${match.player2Score.gameWins}</td> </tr>
+      <c:if test="${match.matchStatus != MatchStatus.TieBrake}">
+        <tr> <th>Games</th> <td>${match.player1Score.gameWins}</td> <td>${match.player2Score.gameWins}</td> </tr>
+      </c:if>
+      <c:if test="${match.matchStatus == MatchStatus.TieBrake}">
+        <tr> <th>Games</th> <td>Tie</td> <td>Brake</td> </tr>
+      </c:if>
       <tr> <th>Score</th> <td>${match.player1Score.score}</td> <td>${match.player2Score.score}</td> </tr>
       <tr> <th></th> <td><button name="player" value="player-1-win">${match.player1.name} wins</button></td> <td><button name="player" value="player-2-win">${match.player2.name} wins</button></td> </tr>
     </table>
